e6dab584fd82155daaacdfcc8ed6708b2f9b7050
TakuyaKinoshita <kinotakunn@gmail.com>
2023-03-27 14:37:16 +0900

    create test data on workspaces table

---
+++ src/database/factories/WorkspaceFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Workspace;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Workspace>
+ */
+class WorkspaceFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Workspace::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'customer_id' => $this->faker->numberBetween(1, 50),
+            'workspace_name' => $this->faker->word()
+        ];
+    }
+}
